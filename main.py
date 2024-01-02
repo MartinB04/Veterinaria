e7f420575836afa5159ca7f464ceacbc595af2d8
MartinB04 <martin.bibiano4@outlook.com>
2024-01-02 12:23:46 -0600

    Add: trys y excepts agregados en main.py
    [main.py]
    *Se agregaron los trys y excepts a todos los while True:.
    *Se agrego el case 0: en los match para sustituir los if opc == 0 con break.
    *Se agrego case _: a cada match.
    *Se optimizo el match de actualizar_perfil().

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from Mascota import *
 
 menu = Menu()
 opc = None
-lista_usuarios = []
 lista_mascotas = []
+lista_usuarios = []
 usuario_actual = None
 iofile = IOFile()
+try:
+    lista_usuarios = iofile.lectura_fichero_lista("usuarios")
+except: pass
 
 def registrar_mascota(usuario_actual):
     
@@ -23,19 +26,16 @@ def registrar_mascota(usuario_actual):
     color = input("Color -> ")
 
     mascota = Mascota(nombre, usuario_actual.usuario, tipo_mascota, raza, genero, fecha_nacimiento, peso, color)
-    print(str(mascota))
+    #print(str(mascota))
     lista_mascotas.append(mascota)
     iofile.escritura_fichero_registro(mascota, "mascotas")
     
 def mis_mascotas(usuario_actual):
-
-    if cargar_mascotas_usuario(usuario_actual):
-        
+    if cargar_mascotas_usuario(usuario_actual): 
         for i, mascota in enumerate(lista_mascotas, start=1):
             if(mascota.dueño == usuario_actual.usuario):
                 print(f"\nMascota #{i}")
                 print(str(mascota))
-    
     else:
         print("Error, no tienes mascotas registradas")
         
@@ -48,78 +48,70 @@ def cargar_mascotas_usuario(usuario_actual):
 
 def mascotas(usuario_actual):
     while True:
-        menu.menu_titulo()
-        menu.menu_mascotas()
-        opc_aux = menu.opc
+        try:
+            menu.menu_titulo()
+            menu.menu_mascotas()
+            opc_aux = menu.opc
         
-        match opc_aux:
-            case 1:
-                mis_mascotas(usuario_actual)
-            case 2:
-                registrar_mascota(usuario_actual)
-            case 3:
-                pass
-            case 4:
-                pass
-        
-        if opc_aux == 0:
-            break
+            match opc_aux:
+                case 1: mis_mascotas(usuario_actual)
+                case 2: registrar_mascota(usuario_actual)
+                case 3: pass
+                case 4: pass
+                case 0:
+                    print("Error, opcion fuera de rango.")
+                    break
+                case _: print("Error, opcion fuera de rango.")
+                
+        #if opc_aux == 0:
+            #break
+        except: print("Error, opcion invalida.")
 
 def actualizar_perfil(usuario_actual):    
     while True:
-        menu.menu_titulo()
-        menu.menu_acutalizar_perfil_usuario()
-        opc_aux = menu.opc
-        value = None
+        try:
+            menu.menu_titulo()
+            menu.menu_acutalizar_perfil_usuario()
+            opc_aux = menu.opc
         
-        match opc_aux:
+            match opc_aux:
+                case 1: usuario_actual.usuario = input("User -> ")
+                case 2: usuario_actual.nombre = input("Nombre -> ")
+                case 3: usuario_actual.password = input("Password -> ")
+                case 4: usuario_actual.genero = input("Genero -> ")
+                case 5: usuario_actual.telefono = input("Telefono -> ")
+                case 6: usuario_actual.email = input("Email -> ")
+                case 0: 
+                    print("Regresando.")
+                    break   
+                case _: print("Error, opcion fuera de rango.")
+                
+            iofile.modificar_registro(lista_usuarios, "usuarios")
 
-            case 1:
-                value = input("User -> ")
-                usuario_actual.usuario = value
-                
-            case 2:
-                value = input("Nombre -> ")
-                usuario_actual.nombre = value
-                
-            case 3:
-                value = input("Password -> ")
-                usuario_actual.password = value
-                
-            case 4:
-                value = input("Genero -> ")
-                usuario_actual.genero = value
-                
-            case 5:
-                value = input("Telefono -> ")
-                usuario_actual.telefono = value
-                
-            case 6:
-                value = input("Email -> ")
-                usuario_actual.email = value
-                
-        iofile.modificar_registro(lista_usuarios, "usuarios")
-
-        if opc_aux == 0:
-            break
+            #if opc_aux == 0:
+                #break
+        
+        except: print("Error, opcion invalida.")
 
 def sesion_iniciada(usuario_actual):
     while True:
-        menu.menu_titulo()
-        menu.menu_sesion_iniciada(usuario_actual.nombre)
-        opc_aux = menu.opc
-        match opc_aux:
-            case 1:
-                pass
-            case 2:
-                pass
-            case 3:
-                mascotas(usuario_actual)
-            case 4: 
-                actualizar_perfil(usuario_actual)
+        try:
+            menu.menu_titulo()
+            menu.menu_sesion_iniciada(usuario_actual.nombre)
+            opc_aux = menu.opc
+            match opc_aux:
+                case 1: pass
+                case 2: pass
+                case 3: mascotas(usuario_actual)
+                case 4: actualizar_perfil(usuario_actual)
+                case 0: 
+                    print("Cerrando sesion.")
+                    break
+                case _: print("Error, opcion fuera de rango.")
                 
-        if opc_aux == 0:
-            break
+            #if opc_aux == 0:
+                #break
+        except: print("Error, opcion invalida.")
 
 def iniciar_sesion():
     if len(lista_usuarios) > 0 :
@@ -132,10 +124,10 @@ def iniciar_sesion():
                 usuario_actual = u
                 sesion_iniciada(usuario_actual)
             else:
-                print("No existe el usuario ingresado.")
+                print("Error, usuario y/o contraseña invalidos.")
     
     else:
-        print("Error, no hay lista_usuarios registrados")
+        print("Error, lista de usuarios vacia")
             
 def registrar_usuario():
     user = input("User -> ")
@@ -146,35 +138,35 @@ def registrar_usuario():
     email = input("Email -> ")
     usuario = Usuario(user, nombre, password, genero, telefono, email)
     lista_usuarios.append(usuario)
-    print(str(usuario))
+    #print(str(usuario))
     iofile.escritura_fichero_registro(usuario, "usuarios")
 
 def buscar_usuario():
     if len(lista_usuarios)>0:
-        user = input("User a buscar -> ")
+        user = input("Usuario a buscar -> ")
 
         for u in lista_usuarios: 
             if u.usuario == user:
                 print(str(u))
             else:
-                print("No existe el usuario ingresado.")
+                print("Error, no existe el usuario buscado.")
     else:
-        print("Error, no hay lista_usuarios registrados")     
+        print("Error, lista de usuario vacia")     
         
-
-lista_usuarios = iofile.lectura_fichero_lista("usuarios")
-
 while(True):
-    menu.menu_titulo()
-    menu.menu_principal()
-    opc = menu.opc
+    try:
+        menu.menu_titulo()
+        menu.menu_principal()
+        opc = menu.opc
     
-    match(opc):
-        case 1: iniciar_sesion()
-            
-        case 2: registrar_usuario()
-            
-        case 3: buscar_usuario()
-            
-    if(opc == 0):
-        break
+        match(opc):
+            case 1: iniciar_sesion()
+            case 2: registrar_usuario()
+            case 3: buscar_usuario()
+            case 0: 
+                print("Saliendo del programa.")
+                break
+            case _: print("Error, opcion fuera de rango.")
+                    
+    except:
+        print("Error, opcion invalida.")
